fcee664d9896e5e0b1bf43ccff9120e32fd3606b
DiogoCarapito <diogo.carapito@icloud.com>
2025-09-04 16:35:56 +0100

    removed pytest from github actions

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,10 @@
 # pylint: disable=E0611, W0612
 
-import os
-import pytest
-
 from PySide6.QtWidgets import QApplication
 from main import MainWindow
 
 
 def test_main_window_title():
-    if os.environ.get("CI") == "true" or os.environ.get("GITHUB_ACTIONS") == "true":
-        pytest.skip("Skipping GUI test in CI environment")
     app = QApplication.instance() or QApplication([])
     window = MainWindow()
     assert window.windowTitle() == "PySide6 template"
